07bb9e447233567467c15b382805de72a3140834
Emiliano Daian <emilianodaian@gmail.com>
2023-02-22 15:37:00 -0300

    Creacion de Rutas y Modulos del Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './components/shared/shared.module';
 
-//Angular Material
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 
 @NgModule({
@@ -24,12 +20,9 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     ReactiveFormsModule,
-    MatSnackBarModule,
-    MatProgressSpinnerModule
+    SharedModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
